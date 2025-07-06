027cc510a93b7402b2b1d9bd1005475931c53576
박준희 <wnsgmlb501@naver.com>
2025-07-06 22:05:14 +0900

    nav 1차 수정 완

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import './Nav.css';
+
+
+function Nav() {
+  return (
+    <nav className="nav-container">
+      {/* 로고 표시 */}
+      <div className="nav-left">
+        <Link to="/" className="nav-logo">logo(SBC)</Link>
+      </div>
+
+      {/* 기능별 집합 */}
+      <div className="nav-right"> 
+        <Link to="/star" className="nav-item">star</Link>
+        <Link to="/contact" className="nav-item">contact</Link>
+        <Link to="/write" className="nav-item">write</Link>
+        <Link to="/login" className="nav-item">login</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav'
 
 function Home() {
   return (
     <div>
+      <Nav/>
       <h1>🏠 홈</h1>
       <p>반려동물 임시보호 매칭 플랫폼에 오신 것을 환영합니다.</p>
     </div>
