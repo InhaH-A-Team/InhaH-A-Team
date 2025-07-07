6d9263194d62c1fc3f84a36f389b812aa6667644
박준희 <wnsgmlb501@naver.com>
2025-07-07 21:56:33 +0900

    Nav 크기 조정(60->45), Postform.jsx 컴포넌트 및 css 작성

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Write from './pages/Write';
 import Details from './pages/Details';
 import MyPage from './pages/MyPage';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />        
         <Route path="/write" element={<Write />} />
         <Route path="/details" element={<Details />} />
         <Route path="/mypage" element={<MyPage />} />
