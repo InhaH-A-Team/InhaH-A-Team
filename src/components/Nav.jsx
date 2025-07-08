de6e11b69a29a32a83cbd7177c9e0a4d1ee66801
june <wnsgmlb501@naver.com>
2025-07-08 18:54:50 +0900

    about페이지 1차 작성안

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ function Nav() {
       {/* 기능별 집합 */}
       <div className="nav-right"> 
         <Link to="/star" className="nav-item">star</Link>
-        <Link to="/contact" className="nav-item">contact</Link>
+        <Link to="/About" className="nav-item">About us</Link>
         <Link to="/write" className="nav-item">write</Link>
         <Link to="/login" className="nav-item">login</Link>
       </div>
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../components/Nav'
+import Aboutform from '../components/Aboutform'
+
+function About() {
+  return (
+    <div>
+      <Nav/>
+      <Aboutform/>
+    </div>
+  );
+}
+
+export default About;
