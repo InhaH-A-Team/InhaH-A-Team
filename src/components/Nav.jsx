9b68657da267c6bc2e44c3084b73ca1061e83feb
박준희 <wnsgmlb501@naver.com>
2025-07-09 18:51:58 +0900

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,8 +14,8 @@ function Nav() {
 
       {/* 네비게이션 메뉴 */}
       <div className="nav-right"> 
-        <Link to="/star" className="nav-item">star</Link>
         <Link to="/contact" className="nav-item">contact</Link>
+        <Link to="/star" className="nav-item">star</Link>
         <Link to="/write" className="nav-item">write</Link>
         <Link to="/login" className="nav-item">login</Link>
       </div>
